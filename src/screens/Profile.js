c905100115aceca5e86a587d3df3420f0de73e7e
Óðinn Dagur Bjarnason <36035737+odinndagur@users.noreply.github.com>
2022-02-02 16:31:56 +0000

    m

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Layout, Text } from 'react-native-rapi-ui';
 
 export default function ({ navigation }) {
@@ -12,6 +12,21 @@ export default function ({ navigation }) {
 					justifyContent: 'center',
 				}}
 			>
+				<FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
 				<Text>This is the Profile tab</Text>
 			</View>
 		</Layout>
